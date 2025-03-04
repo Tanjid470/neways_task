d1fb5b1c57ab4c1ba02b68653684fca4920553f5
Tanjid470 <tanjidamran11@gmail.com>
2025-03-04 15:02:03 +0600

    feat : add login view and change initial route

--- lib/route/app_pages.dart
+++ lib/route/app_pages.dart
@@ -2,19 +2,26 @@
 import 'package:flutter/animation.dart';
 import 'package:get/get.dart';
 import 'package:neways_task/feature/home/presentation/home_view.dart';
+import 'package:neways_task/feature/login/presentation/login_view.dart';
 import 'package:neways_task/main.dart';
 part 'app_routes.dart';
 class AppPages{
   AppPages._();
 
   static String INITIAL = isInitScreen == 0 || isInitScreen == null
-      ? Routes.homeScreen
+      ? Routes.loginView
       : Routes.homeScreen;
   static final routes =[
     GetPage(
       name:_Paths.homeScreen,
       page: () => const HomeView(),
       transition: Transition.leftToRight,
+      curve: Curves.fastOutSlowIn,
+    ),
+    GetPage(
+      name:_Paths.loginView,
+      page: () =>  const LoginView(),
+      transition: Transition.rightToLeft,
       curve: Curves.fastOutSlowIn,
     ),
   ];
--- lib/route/app_routes.dart
+++ lib/route/app_routes.dart
@@ -2,9 +2,11 @@ part of 'app_pages.dart';
 abstract class Routes {
   Routes._();
   static const homeScreen = _Paths.homeScreen;
+  static const loginView = _Paths.loginView;
 }
 
 abstract class _Paths {
   _Paths._();
   static const homeScreen = '/home_screen';
+  static const loginView = '/login_view';
 }
